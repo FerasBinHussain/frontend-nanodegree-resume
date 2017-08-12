17aac975c1e7dd82a3968c2513c5c101e9e3e7c7
ferasbinhussain <ferasbinhussain@gmail.com>
2017-08-12 12:17:21 +0300

    the completed version

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -43,90 +43,13 @@ var bio = {
     }
   }
 };
-var work = {
-  jobs: [
-    {
-      employer: 'King Saud Uni',
-      title: 'Computer Data Science',
-      location: 'Riyadh',
-      dates: 'September 2013 - Until now',
-      description: "Data science is a 'concept to unify statistics, data analysis and their related methods' in order to 'understand and analyze actual phenomena' with data. It employs techniques and theories drawn from many fields within the broad areas of mathematics, statistics, information science, and computer science, in particular from the subdomains of machine learning, classification, cluster analysis, data mining, databases, and visualization."
-    },
-    {
-      employer: 'King Saud Uni',
-      title: 'Student Console',
-      location: 'Riyadh',
-      dates: 'September 2015 - Until now',
-      description: "like making student live in the univectiy easer"
-          }
-  ],
- 
-  display: function() {
-    if (work.jobs.length > 0) {
-      for (var i = 0, len = work.jobs.length; i < len; i++) {
-        var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[i].employer);
-        var formattedTitle = HTMLworkTitle.replace('%data%', work.jobs[i].title);
-        var formattedDates = HTMLworkDates.replace('%data%', work.jobs[i].dates);
-        var formattedLocation = HTMLworkLocation.replace('%data%', work.jobs[i].location);
-        var formattedDescription = HTMLworkDescription.replace('%data%', work.jobs[i].description);
-        var formattedEmployerTitle = formattedEmployer + formattedTitle;
 
-        $('#workExperience').append(HTMLworkStart);
-        $('.work-entry:last').append(formattedEmployerTitle);
-        $('.work-entry:last').append(formattedDates);
-        $('.work-entry:last').append(formattedLocation);
-        $('.work-entry:last').append(formattedDescription);
-      }
-    }
-  }
-};
-var projects = {
-  projects: [
-    {
-      'title': 'Udacity Portfolio Site',
-      'dates': 'Jone 2017',
-      'description': 'This project is the first project of the Udacity Front-end Nonodegree Program. It is a simple one page portfolio featuring responsive design using the bootstrap framework.',
-      'images': ['images/rsz_portfolio1.png', 'images/rsz_portfolio2.png']
-    },
-    
-    {
-      'title': 'QuizApp - android app',
-      'dates': 'march 2017',
-      'description': 'It was the third app in Android Application Development by udacity',
-      'images': ['images/rsz_quizapp.png']
-    },
-    {
-      'title': 'Tour Guide - android app',
-      'dates': 'April 2017',
-      'description': 'It was the sixth app in Android Application Development by udacity.',
-      'images': ['images/rsz_guidetour.png']
-    }
-  ],
-  display: function() {
-    if (projects.projects.length > 0) {
-      for (var i = 0; i < projects.projects.length; i++) {
-        var formattedTitle = HTMLprojectTitle.replace('%data%', projects.projects[i].title);
-        var formattedDates = HTMLprojectDates.replace('%data%', projects.projects[i].dates);
-        var formattedDescription = HTMLprojectDescription.replace('%data%', projects.projects[i].description);
+bio.display();
 
-        $('#projects').append(HTMLprojectStart);
-        $('.project-entry:last').append(formattedTitle);
-        $('.project-entry:last').append(formattedDates);
-        $('.project-entry:last').append(formattedDescription);
-
-        for (var j = 0; j < projects.projects[i].images.length; j++) {
-          var formattedImage = HTMLprojectImage.replace('%data%', projects.projects[i].images[j]);
-
-          $('.project-entry:last').append(formattedImage);
-        }
-      }
-    }
-  }
-};
 var education = {
   schools: [
     {
-      'name': 'KSU -King Saud univectiy',
+      'name': 'KSU - King Saud univectiy',
       'location': 'almuzahmyah, Riyadh',
       'degree': 'Bachelor of Computer, Data Science',
       'majors': ['Data Science'],
@@ -180,7 +103,94 @@ var education = {
     }
   }
 };
-bio.display();
-work.display();
-projects.display();
+
 education.display();
+
+var work = {
+  jobs: [
+    {
+      employer: 'King Saud Uni',
+      title: 'Computer Data Science',
+      location: 'Riyadh',
+      dates: 'September 2013 - Until now',
+      description: "Data science is a 'concept to unify statistics, data analysis and their related methods' in order to 'understand and analyze actual phenomena' with data. It employs techniques and theories drawn from many fields within the broad areas of mathematics, statistics, information science, and computer science, in particular from the subdomains of machine learning, classification, cluster analysis, data mining, databases, and visualization."
+    },
+    {
+      employer: 'King Saud Uni',
+      title: 'Student Console',
+      location: 'Riyadh',
+      dates: 'September 2015 - Until now',
+      description: "like making student live in the univectiy easer"
+          }
+  ],
+ 
+  display: function() {
+    if (work.jobs.length > 0) {
+      for (var i = 0, len = work.jobs.length; i < len; i++) {
+        var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[i].employer);
+        var formattedTitle = HTMLworkTitle.replace('%data%', work.jobs[i].title);
+        var formattedDates = HTMLworkDates.replace('%data%', work.jobs[i].dates);
+        var formattedLocation = HTMLworkLocation.replace('%data%', work.jobs[i].location);
+        var formattedDescription = HTMLworkDescription.replace('%data%', work.jobs[i].description);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+        $('#workExperience').append(HTMLworkStart);
+        $('.work-entry:last').append(formattedEmployerTitle);
+        $('.work-entry:last').append(formattedDates);
+        $('.work-entry:last').append(formattedLocation);
+        $('.work-entry:last').append(formattedDescription);
+      }
+    }
+  }
+};
+
+work.display();
+
+var projects = {
+  projects: [
+    {
+      'title': 'Udacity Portfolio Site',
+      'dates': 'Jone 2017',
+      'description': 'This project is the first project of the Udacity Front-end Nonodegree Program. It is a simple one page portfolio featuring responsive design using the bootstrap framework.',
+      'images': ['images/rsz_portfolio1.png', 'images/rsz_portfolio2.png']
+    },
+    
+    {
+      'title': 'QuizApp - android app',
+      'dates': 'march 2017',
+      'description': 'It was the third app in Android Application Development by udacity',
+      'images': ['images/rsz_quizapp.png']
+    },
+    {
+      'title': 'Tour Guide - android app',
+      'dates': 'April 2017',
+      'description': 'It was the sixth app in Android Application Development by udacity.',
+      'images': ['images/rsz_guidetour.png']
+    }
+  ],
+  display: function() {
+    if (projects.projects.length > 0) {
+      for (var i = 0; i < projects.projects.length; i++) {
+        var formattedTitle = HTMLprojectTitle.replace('%data%', projects.projects[i].title);
+        var formattedDates = HTMLprojectDates.replace('%data%', projects.projects[i].dates);
+        var formattedDescription = HTMLprojectDescription.replace('%data%', projects.projects[i].description);
+
+        $('#projects').append(HTMLprojectStart);
+        $('.project-entry:last').append(formattedTitle);
+        $('.project-entry:last').append(formattedDates);
+        $('.project-entry:last').append(formattedDescription);
+
+        for (var j = 0; j < projects.projects[i].images.length; j++) {
+          var formattedImage = HTMLprojectImage.replace('%data%', projects.projects[i].images[j]);
+
+          $('.project-entry:last').append(formattedImage);
+        }
+      }
+    }
+  }
+};
+  
+projects.display();
+
+$('#mapDiv').append(googleMap);
+
